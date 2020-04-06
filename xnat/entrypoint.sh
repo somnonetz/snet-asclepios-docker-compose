ded60373dfbad2a2aec9b2ae3cf75ec1153e942f
jmz-b <jaymuhz.bee@gmail.com>
2020-04-06 10:57:27 +0200

    make xnat config on container start
    the xnat config was being generated during the image build phase, this
    meant that hardcoded passwords were present in the image.
    
    this change moves the xnat config gen to the containers entrypoint. now
    it is possible to safely publish the image

--- xnat/entrypoint.sh
+++ xnat/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -e
+
+cmd="$@"
+
 # generate xnat config
 if [ ! -f $XNAT_HOME/config/xnat-conf.properties ]; then
   cat > $XNAT_HOME/config/xnat-conf.properties << EOF
@@ -15,3 +19,11 @@ hibernate.cache.use_query_cache=true
 EOF
 fi
 
+# wait for postgres
+until psql -U "$XNAT_DATASOURCE_USERNAME" -h xnat-db -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 5
+done
+
+>&2 echo "Postgres is up - executing command \"$cmd\""
+exec $cmd
